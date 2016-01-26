2df12746cbd54951f46511b4a7da1b157c314964
thegeolawton <geofry.m.lawton@gmail.com>
2016-01-26 17:47:23 -0500

    Still broken. Need to fix reading X_train.txt

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,18 @@
 #Preparation of Samsung Movement Data for analysis.
 #Data available from here: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
-read.custom <- function(x){
-        read.table(x,sep=" ")
-}
+#read.custom <- function(x){
+#        read.table(x,sep=" ")
+#}
 #Read in All Data
 #Read y_train
-ytrain <- read.custom("train/y_train.txt")
+#ytrain <- read.table("train/y_train.txt", sep = " ")
 #Read X_train
-xtrain <- read.custom("train/X_train.txt")
+xtrain <- read.table("train/X_train.txt", sep = " ")
 #Read subject_train
-subtrain <- read.custom("train/suject_train.txt")
+#subtrain <- read.table("train/subject_train.txt", sep = " ")
 #Read y_test
-ytest <- read.custom("test/y_test.txt")
+#ytest <- read.table("test/y_test.txt", sep = " ")
 #Read x_test
-xtest <- read.custom("test/X_test.txt")
+#xtest <- read.table("test/X_test.txt", sep = " ")
 #Read subject_test
-subtest <- read.custom("test/subject_test.txt")
+#subtest <- read.table("test/subject_test.txt", sep = " ")
