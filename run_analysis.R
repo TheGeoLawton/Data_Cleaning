76e944aaffb4ba6b04ab3be60a65c15010a43aec
thegeolawton <geofry.m.lawton@gmail.com>
2016-01-27 07:28:35 -0500

    Fixed reading issues, added naming of columns

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,31 @@
 #Preparation of Samsung Movement Data for analysis.
 #Data available from here: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
-#read.custom <- function(x){
-#        read.table(x,sep=" ")
-#}
+library(dplyr)
+library(tidyr)
 #Read in All Data
 #Read y_train
-#ytrain <- read.table("train/y_train.txt", sep = " ")
+ytrain <- read.table("train/y_train.txt", sep = "")
 #Read X_train
-xtrain <- read.table("train/X_train.txt", sep = " ")
+xtrain <- read.table("train/X_train.txt", sep = "")
 #Read subject_train
-#subtrain <- read.table("train/subject_train.txt", sep = " ")
+subtrain <- read.table("train/subject_train.txt", sep = "")
 #Read y_test
-#ytest <- read.table("test/y_test.txt", sep = " ")
+ytest <- read.table("test/y_test.txt", sep = "")
 #Read x_test
-#xtest <- read.table("test/X_test.txt", sep = " ")
+xtest <- read.table("test/X_test.txt", sep = "")
 #Read subject_test
-#subtest <- read.table("test/subject_test.txt", sep = " ")
+subtest <- read.table("test/subject_test.txt", sep = "")
+
+###Bind vertically
+xdat <- bind_rows(xtrain,xtest)
+ydat <- bind_rows(ytrain,ytest)
+subdat <- bind_rows(subtrain,subtest)
+
+#remove test and train dataframes.
+rm("xtest","xtrain","ytest","ytrain","subtest","subtrain")
+
+###Add Descriptive Headers to Data.
+names(xdat) <- read.table("features.txt")[2]
+
+names(ydat) <- "Action Performed"
+names(subdat) <- "Participant"
